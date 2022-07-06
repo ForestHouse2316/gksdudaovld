9c9394448749ab0b5b51e3e3720e60e9404bdd55
ForestHouse <foresthouse2316@gmail.com>
2022-07-06 17:43:44 +0900

    [Build] Change version to 1.0.6b1

--- Python/gksdudaovld/setup.py
+++ Python/gksdudaovld/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='gksdudaovld',
-      version='1.0.6',
+      version='1.0.6b1',
       description='Mapping module for conversion between Korean and English',
       author='ForestHouse',
       author_email='foresthouse2316@gmail.com',
